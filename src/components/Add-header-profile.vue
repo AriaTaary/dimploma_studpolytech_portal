7f5287afe3425e3d893889652e105c3529d413a6
Arina <rina.runet@gmail.com>
2021-06-22 21:31:08 +0300

    fix

--- src/components/Add-header-profile.vue
+++ src/components/Add-header-profile.vue
@@ -17,6 +17,27 @@
         </ul>
       </div>
 
+      <div class="menu-mobile-profile">
+            <Slide right>
+            <!-- <a id="home" href="#">
+              <span>Home</span>
+            </a> -->
+            <ul class="menu-mobile-list-profile-mobile">
+              <router-link
+            v-for="link in links"
+            :key="link.component.name"
+            tag="li"
+            class="menu-item-add-mobile"
+            active-class="menu-item-add-active-mobile"
+            :to="link.component"
+            :exact="link.exact"
+          >
+            <a href="#" class="menu-link-add">{{link.title}}</a>
+          </router-link>
+            </ul>
+            </Slide>
+      </div>
+
 
     </div>
   </div>
@@ -24,8 +45,12 @@
 
 <script>
 import {store} from '@/store'
+import { Slide } from 'vue-burger-menu'
 
   export default {
+    components: {
+      Slide
+  },
     data: () => ({
       links: [
         {title: 'Профиль', component: { name: 'Personal', params: { username: store.getters.getUser.username } }, exact: true},
