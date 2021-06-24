47f817556ee4f04092aa9f1f491674619b037225
Pavel <ozpavel@yandex.ru>
2021-06-24 23:48:36 +0300

    dist

---
+++ dist/precache-manifest.2cbf4a59551efad6c57308afe6ad15bf.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3034abea58d3e9235a0b",
+    "url": "/css/app.927217eb.css"
+  },
+  {
+    "revision": "60c2f11050a48afe139a9a9423af0af8",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "3340e7eee5ba42eb72e5b255aa49f61d",
+    "url": "/img/address.svg"
+  },
+  {
+    "revision": "278817874112273569d7f64ea39c3462",
+    "url": "/img/article-photo.png"
+  },
+  {
+    "revision": "0893e04b3d1f1f1dadc3bc3e2ba21316",
+    "url": "/img/avatar.svg"
+  },
+  {
+    "revision": "c4acc7329d56517c60c3cd69df0ef063",
+    "url": "/img/bookmark-active.svg"
+  },
+  {
+    "revision": "7983e17e3dfbe977bf120b0f5cf97f94",
+    "url": "/img/bookmark.svg"
+  },
+  {
+    "revision": "a8c8f69eee43148ba5ebd50b47746a45",
+    "url": "/img/default_avatar.svg"
+  },
+  {
+    "revision": "2fbecc2dc21b513a5b5e7f34dee6f677",
+    "url": "/img/email.svg"
+  },
+  {
+    "revision": "0311367f530cf2c8b51181710e402e9d",
+    "url": "/img/filter-hover.svg"
+  },
+  {
+    "revision": "a1830e45ffc416d8644ed2184cb0f8da",
+    "url": "/img/filter.svg"
+  },
+  {
+    "revision": "dd33f8b3b36781d150026ef985c1ceb8",
+    "url": "/img/like-active.svg"
+  },
+  {
+    "revision": "675f25d1f487fd19c3a702bd63db9ac9",
+    "url": "/img/like.svg"
+  },
+  {
+    "revision": "2cfc757d624a22fa7f8efb5c49682ef4",
+    "url": "/img/logo.jpg"
+  },
+  {
+    "revision": "fa7fb3298b7aa2ab763740a3176fe5f8",
+    "url": "/img/main-cover-new.png"
+  },
+  {
+    "revision": "6c58f75e098c04b1b7bf858101ebdad1",
+    "url": "/img/main-cover.png"
+  },
+  {
+    "revision": "2a9d8408bf1e3394bf938c108e180e87",
+    "url": "/img/news.jpeg"
+  },
+  {
+    "revision": "42b9115fc4562a774a99d35b9514a532",
+    "url": "/img/no-avatar.png"
+  },
+  {
+    "revision": "f89b069495bc6f1edd7192858e56a556",
+    "url": "/img/phone.svg"
+  },
+  {
+    "revision": "ab7f61de97ef03fb45682482c5b5404c",
+    "url": "/img/polytech-logo-white.svg"
+  },
+  {
+    "revision": "2929352397a6ae7ca80ab05e37b0ed71",
+    "url": "/img/preloader-mini.svg"
+  },
+  {
+    "revision": "aa7a509a84e0597a96638d72b624956d",
+    "url": "/img/preloader.svg"
+  },
+  {
+    "revision": "5e86e3bf37069ac0f2ee1d83b3d805e9",
+    "url": "/img/search.svg"
+  },
+  {
+    "revision": "6780a0a886d9654d47cc921f402c495d",
+    "url": "/img/separator.svg"
+  },
+  {
+    "revision": "cc5b5ac1b17f44eef24bde06711ddc7b",
+    "url": "/img/sort-hover.svg"
+  },
+  {
+    "revision": "14af5f4e3c2b915de601b25d91f86b9f",
+    "url": "/img/sort.svg"
+  },
+  {
+    "revision": "29443596b76fcc3a9c2d58957f353443",
+    "url": "/img/star-active.svg"
+  },
+  {
+    "revision": "5beb0c0b8d699b2384a1707b72dcbcba",
+    "url": "/img/star-hover.svg"
+  },
+  {
+    "revision": "8b96c714149deb7e447a446187fde4bf",
+    "url": "/img/star.svg"
+  },
+  {
+    "revision": "2189ab80e4ce616c3f62821838d022e3",
+    "url": "/img/view.svg"
+  },
+  {
+    "revision": "261d0dde81f652d1869f36fbcb9dc45d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3034abea58d3e9235a0b",
+    "url": "/js/app.827829a7.js"
+  },
+  {
+    "revision": "4686867340c4b198051c",
+    "url": "/js/chunk-vendors.4daa56d7.js"
+  },
+  {
+    "revision": "c556e7a36a0a4f2d469ad2e19b56a197",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
